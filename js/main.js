3d7232a6b0cdced7751ee96e628a43369d220ce1
Fahad Ghaffar <fahad.ghaffar97@gmail.com>
2022-05-08 22:05:10 -0700

    NFT_first_commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+import $ from "./JQuery";
+import "./owl.carousel.min.js";
+import "../css/styles.css";
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:4,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
